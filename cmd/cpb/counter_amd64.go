ba50fbfdf77ff8863595914a81a4ab1fc2637408
Coda Hale <coda.hale@gmail.com>
2026-03-22 15:18:32 -0600

    go fmt

--- cmd/cpb/counter_amd64.go
+++ cmd/cpb/counter_amd64.go
@@ -5,5 +5,5 @@ package main
 //go:noescape
 func rdtsc() uint64
 
-func readCounter() uint64      { return rdtsc() }
+func readCounter() uint64            { return rdtsc() }
 func counterScale(_ float64) float64 { return 1.0 }
--- cmd/cpb/counter_other.go
+++ cmd/cpb/counter_other.go
@@ -2,5 +2,5 @@
 
 package main
 
-func readCounter() uint64 { panic("cycle counter not supported on this platform") }
+func readCounter() uint64            { panic("cycle counter not supported on this platform") }
 func counterScale(_ float64) float64 { panic("cycle counter not supported on this platform") }
